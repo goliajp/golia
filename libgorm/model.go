50172a54cc16d2c91b7362a86abddaac307ed965
panda <lihao@golia.jp>
2020-03-08 20:23:26 +0900

    fix: change Id to ID for golint spec

--- libgorm/model.go
+++ libgorm/model.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Model struct {
-	Id        uint       `json:"id" gorm:"primary_key; index"`
+	ID        uint       `json:"id" gorm:"primary_key; index"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
 	DeletedAt *time.Time `json:"deleted_at" gorm:"index"`
